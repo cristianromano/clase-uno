57e1b673e3550f3c7972818c5c0c9907afce0897
cristianromano <clonazepam24@gmail.com>
2023-03-30 22:21:30 -0300

    ejercicios clase uno

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'clase-uno';
+  edadUno: number = 0;
+  EdadDos: number = 0;
+  res: number = 0;
+  prom: number = 0;
+  cambiarTitulo() {
+    this.title = 'Nuevo titulo';
+  }
+
+  sumar() {
+    this.res = Number(this.edadUno) + Number(this.EdadDos);
+    this.prom = 0;
+  }
+
+  dividir() {
+    this.prom = (Number(this.edadUno) + Number(this.EdadDos)) / 2;
+    this.res = 0;
+  }
+
+  limpiar() {
+    this.edadUno = 0;
+    this.EdadDos = 0;
+    this.res = 0;
+    this.prom = 0;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BienvenidoComponent } from './components/bienvenido/bienvenido.component';
+import { ErrorComponent } from './components/error/error.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BienvenidoComponent,
+    ErrorComponent,
+    LoginComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
